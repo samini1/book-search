7a8fcfdccbfba1b3582a850680d80c5da2c09566
samini1 <92944248+samini1@users.noreply.github.com>
2022-04-06 10:44:11 -0500

    added apolloserver to server.js, need to remove unneeded restful API info

--- Develop/server/server.js
+++ Develop/server/server.js
@@ -2,9 +2,26 @@ const express = require('express');
 const path = require('path');
 const db = require('./config/connection');
 const routes = require('./routes');
+const { ApolloServer } = require('apollo-server-express')
+const { typeDefs, resolvers } = require('./schemas');
+const db = require('./config/connection');
+const { authMiddleware } = require('./utils/auth');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
+
+const startServer = async () => {
+  const server = new ApolloServer({
+    typeDefs,
+    resolvers,
+    context: authMiddleware
+  });
+
+  await server.start();
+  server.applyMiddleware({ app });
+};
+
+startServer();
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
